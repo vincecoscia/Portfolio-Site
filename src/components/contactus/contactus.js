fe8cce1a973a191c322b022df0eb35703e803d4f
Vincent Coscia <xvincentcosciax@gmail.com>
2019-04-30 16:29:08 -0400

    Portfolio site

---
+++ src/components/contactus/contactus.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me for any work or suggestions below
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>
+                <a href="https://www.linkedin.com/in/vincent-coscia-b53b42a8/">Click to contact me via LinkedIn</a>
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,87 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Vince",
+  "role": "Full Stack Web Developer",
+  "linkedinId":"https://www.linkedin.com/in/vincent-coscia-b53b42a8",
+  "roleDescription": " Working with computers has always been my specialty, now developing web applications is my passion.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/vincent-coscia-b53b42a8",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/vincecoscia",
+        "className":"fa fa-github"
+      }
+    ],
+  "aboutme":"After being in sales for a few years and not finding what I was looking for professionally, I decided to pursue a passion of mine and become a Full Stack Web Developer with the help Wyncode Academy. Along the way, I met some incredible people that motivate me to become better each day, whether that be on my craft or life in general. Currently, I've focused my time on expanding my knowledge on front-end development with Javascript and utilizing the React framework.",
+  "address":"Miami, FL, USA",
+  "website":"xvincentcosciax@gmail.com",
+  "education":[
+    {
+      "UniversityName":"Wyncode Academy",
+      "specialization":"Full Stack Web Developer Program",
+      "MonthOfPassing":"March",
+      "YearOfPassing":"2019",
+      "Achievements":"I completed two fantastic projects during my time there: Fútsea, a platform to connect people to soccer fields in the South Florida area with the ability to book right on the site and the PokéDome, which utilizes a pokemon api to find and sort pokemon by different attributes. Met a bunch of great people eager to learn and change their path in life. Learned HTML/CSS, JavaScript, JQuery, React.js, Ruby, and full backend development through Ruby on Rails."
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"The Preiss Company",
+      "specialization":"Real Estate, Sales",
+      "Achievements":"During my time at the Preiss Company, I accounted for about a million dollars of revenue through leasing. While leasing was the top priority, other duties included handling resident issues, file auditing, financial accounting, event planning, project management, marketing, and maintenance."
+    },
+  ],
+  "skillsDescription":"Languages, Frameworks, & Libraries",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"JavaScript"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Ruby"
+    },
+    {
+      "skillname":"Rails"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Miami Ridez",
+      "description":"Very cool Miami Bike Station Search App made with MapBox GL, and citybike API. Send out to your friends to find bikes while on vacay!",
+      "imgurl":"images/portfolio/miamiridez.jpg",
+      "url":"https://miamiridez.netlify.com/"
+    },
+    {
+      "name":"Fútsea",
+      "description":"Web Application, Final Project at Wyncode. Connects users to Soccer fields in South Florida. Integrated with the Stripe API for payments.",
+      "imgurl":"images/portfolio/Futsea.png",
+      "url":"https://futsea.herokuapp.com"
+    },
+    {
+      "name":"PokéDome",
+      "description":"Web Application, can sort and search through all Pokemon using the PokéAPI. Mobile-friendly!",
+      "imgurl":"images/portfolio/Pokedome.png",
+      "url":"https://Pokedome.herokuapp.com"
+    },
+  ],
+  "testimonials":[
+    {
+      "description":"Unbelievably hard working individual with a passion for meeting and exceeding goals!",
+      "name":"Ernest Joe"
+    },
+  ]
+}
+
+export default resumeData
